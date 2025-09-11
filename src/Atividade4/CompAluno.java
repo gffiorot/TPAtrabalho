2f13beb553a8f1860fb8d6cc72dca16ce0db586c
Gffiorot <gffiorot@outlook.com>
2025-09-11 17:20:10 -0300

    Atividade4 completa

--- src/Atividade4/CompAluno.java
+++
@@ -1,9 +0,0 @@
-package Atividade4;
-import java.util.Comparator;
-
-public class CompAluno implements Comparator<Aluno> {
-    @Override
-    public int compare(Aluno o1, Aluno o2) {
-        return o1.getNome().compareToIgnoreCase(o2.getNome());
-    }
-}
